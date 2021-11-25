7acf71a5d9a269cbbd0dbd5ae49b18101cb90a79
Matthias Clein <m.cleinespinoza@gmail.com>
2021-11-25 10:04:39 -0300

    basic config & first test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
+import React, { useEffect, useRef, useState } from "react";
+import ml5 from 'ml5';
+import useInterval from '@use-it/interval';
 import logo from './logo.svg';
 import './App.css';
 
+import Chart from './Chart';
+
+let classifier;
+
 function App() {
+  const videoRef = useRef();
+  const [start, setStart] = useState(false);
+  const [result, setResult] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    classifier = ml5.imageClassifier("./model/model.json", () => {
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: false })
+        .then((stream) => {
+          videoRef.current.srcObject = stream;
+          videoRef.current.play();
+          setLoaded(true);
+        });
+    });
+  }, []);
+
+  useInterval(() => {
+    if (classifier && start) {
+      classifier.classify(videoRef.current, (error, results) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+        setResult(results);
+        console.log(results)
+      });
+    }
+  }, 2000);
+  const toggle = () => {
+    setStart(!start);
+    setResult([]);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Control de asistencia
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <video
+            ref={videoRef}
+            style={{ transform: "scale(-1, 1)" }}
+            width="300"
+            height="150"
+          />
+          {result.length > 0 && (
+          <div>
+            <Chart data={result[0]} />
+          </div>
+        )}
+        <button onClick={() => toggle()}>
+              {start ? "Stop" : "Start"}
+            </button>
       </header>
     </div>
   );
